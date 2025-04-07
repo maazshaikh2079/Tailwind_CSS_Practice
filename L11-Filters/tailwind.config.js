42be87a6307727ffb82662953818c3afbe09e38a
maaz <maazshaikh2079@gmail.com>
2025-04-07 21:47:55 +0530

    5th commit

---
+++ L11-Filters/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+   theme: {
+     extend: {},
+    //  container:{
+    //    center:true
+    //  }
+   },
+   plugins: [],
+  //  corePlugins: {
+  //   filter:false
+  //  }
+}
