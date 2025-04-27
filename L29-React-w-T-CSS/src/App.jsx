7b1ba500509ca240f87c1670f8f230e9c3c839ac
maaz <maazshaikh2079@gmail.com>
2025-04-27 19:01:39 +0530

    15th commit

---
+++ L29-React-w-T-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1 className="bg-amber-500 dark:bg-amber-700 text-3xl font-bold underline font-jetbrains-mono">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
